db17526b6e5c0c3ca7285a0a60e3dc8ecb14f2f7
ikwuh jeffery enokela <ikwuhj@gmail.com>
2022-12-11 20:02:20 +0100

    feat:added new collection type

---
+++ src/api/clasikdrip/controllers/clasikdrip.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  clasikdrip controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::clasikdrip.clasikdrip');
